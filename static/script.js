681513235dcf43fa4cac774f70d8045dd3e579b3
Carter Henderson <carterh062@gmail.com>
2015-12-07 12:08:49 -0500

    js patch

--- static/script.js
+++ static/script.js
@@ -1,11 +1,10 @@
 $('body > div.projects-container > div.project > div.inner').click(function(e){
 	var classClicked = $(e.target).attr('class')
-	var link;
-	if(classClicked == "pic") // An actual link was clicked
-		link = $(e.target).parent().attr('href')
-	else{
+	var link = null;
+	if(classClicked != "pic"){ // An actual link wasn't clicked
 		var $first_link = $($(this).find("div.links").find("a.link").get(0))
 		link = $first_link.attr('href');
 	}
-	window.open(link);
+	if(link)
+		window.open(link);
 })
